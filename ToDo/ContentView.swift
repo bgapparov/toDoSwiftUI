a65c439eef3998c340b1dd3a7eab09028da36a86
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-07-26 21:35:49 -0500

    Created project

---
+++ ToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Baiaman Gapparov on 7/26/20.
+//  Copyright © 2020 Baiaman Gapparov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDo/ToDoItem.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoItem.swift
+//  ToDo
+//
+//  Created by Baiaman Gapparov on 7/26/20.
+//  Copyright © 2020 Baiaman Gapparov. All rights reserved.
+//
+
+import Foundation
